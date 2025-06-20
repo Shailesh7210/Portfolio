04e6df828edcc13062e18f70891898806a074b6a
Shailesh7210 <shailjaiswal9135@gmail.com>
2025-06-20 11:19:40 +0530

    first commit

---
+++ src/Home.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { ThemeToggle } from './Components/ThemeToggle'
+import { StarBackground } from './Components/StarBackground'
+import { Navbar } from './Components/Navbar'
+import {HeroSection} from './Components/HeroSection'
+import {AboutSection} from './Components/AboutSection'
+import {SkillsSection} from './Components/SkillsSection'
+import { ProjectsSection } from './Components/ProjectSection'
+import { ContactSection } from './Components/ContactSection'
+import { Footer } from './Components/Footer'
+
+
+const Home = () => {
+  return (
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
+    {/* {Theme Toggle} */}
+        <ThemeToggle/>
+
+    {/* {Background Effects} */}
+        <StarBackground/>
+
+
+    {/* {Navbar} */}
+       <Navbar/>
+
+
+    {/* {Main Content} */}
+    <main>
+      <HeroSection/>
+      <AboutSection/>
+      <SkillsSection/>
+      <ProjectsSection/>
+      <ContactSection/>
+      <Footer/>
+    </main>
+
+
+    </div>
+  )
+}
+
+export default Home
